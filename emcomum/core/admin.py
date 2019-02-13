19e11e076700b94931bad9d093325677a22c4f76
Guilherme Guerra <guilherme.ga@gmail.com>
2019-02-12 23:48:09 -0200

    Updates email template and add created_at on admin

--- emcomum/core/admin.py
+++ emcomum/core/admin.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 from emcomum.core.models import Meeting
 
 class MeetingModelAdmin(admin.ModelAdmin):
-    list_display = ('host_name', 'guest1_name', 'guest2_name')
+    list_display = ('host_name', 'guest1_name', 'guest2_name', 'created_at')
     date_hierarchy = 'created_at'
 
 admin.site.register(Meeting, MeetingModelAdmin)
