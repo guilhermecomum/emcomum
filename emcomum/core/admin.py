59c6e4946b131221a69c051a2fb254fe3f43ff1b
Guilherme Guerra <guilherme.ga@gmail.com>
2019-02-12 23:20:29 -0200

    Setup admin

--- emcomum/core/admin.py
+++ emcomum/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.utils.timezone import now
+from emcomum.core.models import Meeting
 
-# Register your models here.
+class MeetingModelAdmin(admin.ModelAdmin):
+    list_display = ('host_name', 'guest1_name', 'guest2_name')
+    date_hierarchy = 'created_at'
+
+admin.site.register(Meeting, MeetingModelAdmin)
